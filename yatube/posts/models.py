d8c9f6a114b71bd708b9388eaa04f506b6c4bf71
Тимофей Городилов <timotheos94@protonmail.com>
2022-06-13 02:02:04 +0300

    1st_rewiew_commit_halfway

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,27 +5,32 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    address = models.URLField(blank=True, null=True, max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    title = models.CharField("Название", max_length=200,)
+    address = models.URLField(blank=True, null=True, max_length=200,)
+    slug = models.SlugField("Текст для ссылки", unique=True)
+    description = models.TextField("Описание",)
 
     def __str__(self):
-        return self.title
+        return f'Группа - {self.title}'
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='posts'
-    )
-
+    text = models.TextField('Текст')
+    pub_date = models.DateTimeField(
+        "Дата публикации",
+        auto_now_add=True)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='posts',
+                               verbose_name="Автор",
+                               )
     group = models.ForeignKey(
         Group,
         blank=True,
         null=True,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
+        verbose_name="Группа",
     )
+
+    def __str__(self):
+        return self.text
