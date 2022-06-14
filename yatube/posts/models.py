2d82c2293dced57957b7cf471bf7c915af3162f3
Тимофей Городилов <timotheos94@protonmail.com>
2022-06-14 22:05:34 +0300

    rewiew2 - OK migration issues OK, group.posts works

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,7 +6,6 @@ User = get_user_model()
 
 class Group(models.Model):
     title = models.CharField("Название", max_length=200,)
-    address = models.URLField(blank=True, null=True, max_length=200,)
     slug = models.SlugField("Текст для ссылки", unique=True)
     description = models.TextField("Описание",)
 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from yatube.settings import POST_NUMBER
 from .models import Post, Group
 
 
 def index(request):
     template = 'posts/index.html'
-    post_number = 10  # постов на странице
-    posts = Post.objects.filter()[:post_number]
+    posts = Post.objects.filter()[:POST_NUMBER]
     context = {
         'posts': posts,
     }
@@ -14,14 +14,10 @@ def index(request):
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    group = get_object_or_404(Group, slug=slug)
-
-    post_number = 10  # постов на странице
-
-    posts = Post.objects.filter(group=group)[:post_number]
-    # пока что ничерта не понял про _set.all() :(
+    group_list = get_object_or_404(Group, slug=slug)
+    post_list = group_list.posts.all()[:POST_NUMBER]
     context = {
-        'group': group,
-        'posts': posts,
+        'group': group_list,
+        'posts': post_list,
     }
     return render(request, template, context)
