4bb29c1fb5801ad3e858cb3643f8842668ae06c9
Тимофей Городилов <timotheos94@protonmail.com>
2022-06-20 14:37:01 +0300

    4spr static pages /about/ added

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls', namespace='posts')),
+    path('about/', include('about.urls', namespace='about')),
     path('auth/', include('users.urls', namespace='users')),
     path('auth/', include('django.contrib.auth.urls')),
 ]
